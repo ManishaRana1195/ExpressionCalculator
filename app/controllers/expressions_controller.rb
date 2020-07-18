cc5eaba78c443474fce8f31c450bdcc9eb76e452
manisha1195 <manisha1195@gmail.com>
2020-07-18 16:23:56 -0500

    added calculation part to solve expression

--- app/controllers/expressions_controller.rb
+++ app/controllers/expressions_controller.rb
@@ -1,6 +1,7 @@
 class ExpressionsController < ApplicationController
   def create
     @expression = Expression.new(expression_params)
+    @expression.body = @expression.solve
     @expression.save
     redirect_to :root
   end
--- app/models/expression.rb
+++ app/models/expression.rb
@@ -4,5 +4,70 @@ class Expression < ApplicationRecord
     def send_expressiom
         ExpressionBroadcastJob.perform_later(self)
     end
+
+    def solve()
+        expression = self.body
+        expression = expression.gsub(" ", "")
+       
+        stack_operand, stack_operator = get_operators_operands(expression)
+        
+        if stack_operator.nil? or stack_operand.nil?
+            return nil
+        end
+        if stack_operator.length() >= stack_operand.length()
+            return nil
+        end
+        while stack_operator.length() > 0
+            value_a = stack_operand.pop()
+            value_b = stack_operand.pop()
+            operator = stack_operator.pop()
+            stack_operand.append(calculate(value_a, value_b, operator))
+        end
+        result = stack_operand.pop()
+        final_expression = "#{expression} = #{result}"
+       
+        return final_expression
+    end
+
+    def get_operators_operands(expression)
+        stack_operator = []
+        stack_operand =  []
+        operators = ["+", "/", "-", "*"]
+        length = expression.length
+        index = 0
+        
+        while index < length
+            curr_char = expression[index]
+            
+            if operators.include? curr_char
+                index += 1
+                stack_operator.append(curr_char)
+            elsif numeric(curr_char)
+                number = 0
+                while index < length and numeric(expression[index])
+                    number = (number * 10) + expression[index].to_i
+                    index += 1
+                end
+                stack_operand.append(number)
+            else
+                return nil, nil
+            end 
+        end   
+        stack_operand.reverse()
+        stack_operator.reverse()
+        return stack_operand, stack_operator
+    end
+
+    def calculate(value_a, value_b, operator)
+        return value_a / value_b if operator == "/"
+        return value_a + value_b if operator == "+"
+        return value_a - value_b if operator == "-"
+        return value_a * value_b if operator == "*"
+    end
+
+
+    def numeric(character)
+        Float(character) != nil rescue false
+    end
    
 end
