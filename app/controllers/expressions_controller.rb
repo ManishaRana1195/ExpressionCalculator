962b9d50f6c4894e85d7cff0b4faa1a7773b213b
manisha1195 <manisha1195@gmail.com>
2020-07-18 12:41:17 -0500

    added basic view of creating and posting a message

--- app/controllers/expressions_controller.rb
+++ app/controllers/expressions_controller.rb
@@ -1,4 +1,11 @@
 class ExpressionsController < ApplicationController
   def create
+    @expression = Expression.new(expression_params)
+    @expression.save
+  end
+
+  @private
+  def expression_params
+    params.require(:expression).permit(:body)
   end
 end
